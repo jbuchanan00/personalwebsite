3f18beb26c05e0b015ff4da07f95817a8c1ff81c
josh buchanan <josh.buchanan00@gmail.com>
2020-01-25 20:22:25 -0700

    added iframe for resume

---
+++ src/Components/Landing.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+function Landing(){
+    function GithubLink(){
+        console.log("Github Link")
+        window.open("https://github.com/jbuchanan00")
+      }
+      function ResumeLink(){
+        console.log("Resume button")
+        window.open("/resume")
+      }
+      function LinkedInLink(){
+        console.log("Linkedin Link")
+        window.open("https://linkedin.com/in/joshua-g-buchanan")
+      }
+    return(
+            <div className="App">
+      <div className="MaxScreen">
+        <header className="App-header">
+          <h1 className="HeaderName">
+            Josh Buchanan
+          </h1>
+        </header>
+        <div className="NonHeaderCont">
+          <summary className="InfoContainer">
+            <div className="ProfOrPers">
+              Professional or Personal 
+            </div>
+            <p className="TextContainer">
+            </p>
+          </summary>
+        </div>
+          <footer className="LinkContainer">
+            <button onClick = {e => GithubLink()}>
+              GitHub
+            </button>
+            <button onClick={e => ResumeLink()}>
+              Resume
+            </button>
+            <button onClick = {e => LinkedInLink()}>
+              LinkedIn
+            </button>
+          </footer>
+      </div>
+    </div>
+    );
+}
+export default Landing
--- src/Components/ResumeViewer.js
+++ src/Components/ResumeViewer.js
@@ -1,8 +1,14 @@
-import React from "react"
-import { PdfViewerComponent, Toolbar, Magnification, Navigation, LinkAnnotation, BookmarkView, ThumbnailView, Print, TextSelection, Annotation, TextSearch, Inject } from '@syncfusion/ej2-react-pdfviewer';
+import React from 'react';
+import '../App.css'
 
-function ResumeViewer(){
-    
+function ResumeViewer() {
+    return (
+        <div className="ResumeContainer">
+            <iframe className = "ResumeView" src="https://docs.google.com/document/d/e/2PACX-1vTTmW0jfJ1ejWxr621Ib0_tw0d7R40aZWFA7jlT6Qu1WAVeFUzXiRKsOfbR-5XWU87g__uSCp295R-K/pub?embedded=true">
+              Resume
+            </iframe>
+        </div>
+    );
 }
 
 export default ResumeViewer;
